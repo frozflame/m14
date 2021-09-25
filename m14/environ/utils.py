b9019068cf03a1955da395320233990baa7ebb50
frozflame <frozflame@outlook.com>
2021-09-25 17:13:40 +0800

    move check_{inclusive,exclusive}_* functions to joker.environ.utils

--- m14/environ/utils.py
+++ m14/environ/utils.py
@@ -1,53 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import fnmatch
-import importlib
 import logging
-import re
 
-from volkanic.introspect import find_all_plain_modules
-from volkanic.utils import printerr
+from joker.environ.utils import *  # noqa
 
 
-def multicheck(func, target, rules, positive_val: bool) -> bool:
-    for rule in rules:
-        if func(target, rule):
-            return positive_val
-    return not positive_val
-
-
-def check_inclusive_prefixes(string: str, prefixes) -> bool:
-    return multicheck(str.startswith, string, prefixes, True)
-
-
-def check_exclusive_prefixes(string: str, prefixes) -> bool:
-    return multicheck(str.startswith, string, prefixes, False)
-
-
-def check_inclusive_patterns(string: str, patterns, case=True) -> bool:
-    func = fnmatch.fnmatchcase if case else fnmatch.fnmatch
-    return multicheck(func, string, patterns, True)
-
-
-def check_exclusive_patterns(string: str, patterns, case=True) -> bool:
-    func = fnmatch.fnmatchcase if case else fnmatch.fnmatch
-    return multicheck(func, string, patterns, False)
-
-
-def _regex_search(string: str, regex: str):
-    return re.search(regex, string)
-
-
-def check_inclusive_regexes(string: str, patterns) -> bool:
-    return multicheck(_regex_search, string, patterns, True)
-
-
-def check_exclusive_regexes(string: str, patterns) -> bool:
-    return multicheck(_regex_search, string, patterns, True)
-
-
-# deprecated. use check_{inclusive,exclusive}_* functions
+# deprecated. use joker.environ.utils.check_{inclusive,exclusive}_* functions
 def check_prefixes(string: str, include=None, exclude=None) -> bool:
     """
     Args:
@@ -66,15 +25,6 @@ def check_prefixes(string: str, include=None, exclude=None) -> bool:
         return False
     else:
         return True
-
-
-def load_all_modules(project_dir: str, *dotpath_prefixes):
-    if not project_dir or not dotpath_prefixes:
-        return
-    for dotpath in find_all_plain_modules(project_dir):
-        if check_inclusive_prefixes(dotpath, dotpath_prefixes):
-            printerr('importing', dotpath)
-            importlib.import_module(dotpath)
 
 
 def easylog(level):
