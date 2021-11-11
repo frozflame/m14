be18cb6a1557ac39602075e2a7e374dc1f97ebab
frozflame <frozflame@outlook.com>
2021-11-11 19:36:03 +0800

    m14.experimental: utilities, unstable APIs -- archives.py, datafiles.py, regex.py, taskmgr.py

--- m14/experimental/regex.py
+++ m14/experimental/regex.py
@@ -2,13 +2,12 @@
 # coding: utf-8
 
 import re
-from collections import deque
 
 
-class RegexGroup(object):
+class RegexTester(object):
     def __init__(self, regexes=None, strings=None):
-        self.regexes = deque(regexes or [])
-        self.strings = deque(strings or [])
+        self.regexes = regexes or []
+        self.strings = strings or []
 
     def check_all_regexes(self, string):
         return [r.search(string) for r in self.regexes]
@@ -17,3 +16,4 @@ class RegexGroup(object):
         return [re.search(pattern, s) for s in self.strings]
 
 
+RegexGroup = RegexTester
